447f7ecbeb4959e8fa21d9068565549138e79baa
collectorD <collectorD@yandex.com>
2015-04-13 11:45:51 +0200

    Create collection.js

---
+++ collection.js
@@ -0,0 +1,16 @@
+[
+    { "moduleId" : "mapman100/Blitzer"},
+    
+    { "moduleId" : "supermap01/verkehrsinfo.de"},
+    
+    { "moduleId" : "jamesbond002/fixTanken"},
+    
+    { "moduleId" : "mapman100/Here"},
+    
+    { "moduleId" : "wazemap/waze"},
+    
+    { "moduleId" : "wikifan/wikimap"},
+    
+    { "moduleId" : "grabowCommuter/Speedometer"}
+    
+]
